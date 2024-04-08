3751c8a41d5943c45cd28008f977b39ce3d32f7a
gaeun <gaeun1811@naver.com>
2024-04-08 16:08:12 +0900

    :sparkles: CORS 설정

---
+++ backend/spring-mungtique/user/src/main/java/com/mung/mungtique/member/infrastructure/config/CorsMvcConfig.java
@@ -0,0 +1,17 @@
+package com.mung.mungtique.member.infrastructure.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 인증과 인가가 필요하지 않은 요청에 대해 CORS 적용
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
