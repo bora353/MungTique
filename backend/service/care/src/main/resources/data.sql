1355d07d7f2a99855a00209948e5488043eceaea
gaeun <gaeun1811@naver.com>
2024-07-21 21:26:23 +0900

    :sparkles: DB모두 postgres로 변경 (도커 사용)

--- backend/service/care/src/main/resources/data.sql
+++ backend/service/care/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO mung_shop (storeName, storeAddress, breeds, businessHours, closingDays, latitude, longitude, filePath)
+INSERT INTO mung_shop (store_name, store_address, breeds, business_hours, closing_days, latitude, longitude, file_path)
 VALUES
     ('즐거운 애견 미용실', '경기 화성시 동탄공원로1길 22-3 1층 101호', '모든 견종', '10:00 AM - 18:30 PM', '휴무없음', '37.2031802', '127.060017','/images/mungshop/shop1.jpg'),
     ('멍샬롬', '경기 화성시 동탄공원로 36 다경프라자 1층', '모든 견종', '10:30 AM - 20:00 PM', '일요일', '37.202773', '127.0596957','/images/mungshop/shop2.jpg'),
@@ -6,12 +6,12 @@ VALUES
     ('센트럴동물병원', '경기 화성시 동탄지성로 102 하나로프라자', '모든 견종', '9:30 AM - 20:00 PM', '일요일', '37.2081031', '127.0641932','/images/mungshop/shop4.jpg'),
     ('멍화만사성', '경기 화성시 동탄지성로 143 2층', '모든 견종', '8:00 AM - 20:30 PM', '화요일', '37.2095214', '127.0595995','/images/mungshop/shop5.jpg');
 
-INSERT INTO mymung (mungName, breed, weight, age, gender, fixed, userId)
+INSERT INTO mymung (mung_name, breed, weight, age, gender, fixed, user_id)
 VALUES
     ('싼쵸', '치와와', 4, 5, '수컷', 'Yes', 1),
     ('코코', '말티푸', 2, 1, '암컷', 'No', 1);
 
-INSERT INTO mung_shop_price (breeds, serviceType, price, mung_shop_id)
+INSERT INTO mung_shop_price (breeds, service_type, price, mung_shop_id)
 VALUES ('Golden Retriever', 'Basic grooming', 50000, 1),
        ('Labrador Retriever', 'Full grooming', 80000, 1),
        ('Poodle', 'Basic grooming', 60000, 1),
