3c9e246652e710d329c6439557bcd4cdb2013ae6
bora353 <gaeun1811@naver.com>
2025-02-25 16:15:10 +0900

    fix: N+1 문제 EntityGraph로 해결

--- backend/service/mungshop/src/main/java/com/mung/mungtique/mungshop/adaptor/out/persistence/mungshoprepo/MungShopRepo.java
+++ backend/service/mungshop/src/main/java/com/mung/mungtique/mungshop/adaptor/out/persistence/mungshoprepo/MungShopRepo.java
@@ -1,11 +1,17 @@
 package com.mung.mungtique.mungshop.adaptor.out.persistence.mungshoprepo;
 
 import com.mung.mungtique.mungshop.domain.MungShop;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface MungShopRepo extends JpaRepository<MungShop, Long> {
+
+    @EntityGraph(attributePaths = {"prices"})
+    @NonNull
+    List<MungShop> findAll();
 
     List<MungShop> findByStoreNameContaining(String searchQuery);
 }
