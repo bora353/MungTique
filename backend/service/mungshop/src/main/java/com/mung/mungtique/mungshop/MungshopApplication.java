8fd7362d6fbdfba710d854f784aefa045b103ee8
bora353 <gaeun1811@naver.com>
2025-03-13 18:58:26 +0900

    chore

--- backend/service/mungshop/src/main/java/com/mung/mungtique/mungshop/MungshopApplication.java
+++ backend/service/mungshop/src/main/java/com/mung/mungtique/mungshop/MungshopApplication.java
@@ -2,8 +2,10 @@ package com.mung.mungtique.mungshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MungshopApplication {
 
     public static void main(String[] args) {
