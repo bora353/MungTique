ead4f0f550d0ba6ff919adef6a834b354b9b5f77
bora353 <gaeun1811@naver.com>
2025-03-21 17:30:24 +0900

    feat: 예약 요청 후 10분 지나면 다시 예약 가능하게 작동하는 스케쥴러 추가

--- backend/service/mungshop/src/main/java/com/mung/mungtique/mungshop/adaptor/in/scheduler/ReservationCleanupScheduler.java
+++ backend/service/mungshop/src/main/java/com/mung/mungtique/mungshop/adaptor/in/scheduler/ReservationCleanupScheduler.java
@@ -1,4 +1,22 @@
 package com.mung.mungtique.mungshop.adaptor.in.scheduler;
 
+
+import com.mung.mungtique.mungshop.application.port.in.MungShopReservationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class ReservationCleanupScheduler {
+
+    private final MungShopReservationService mungShopReservationService;
+
+    @Scheduled(fixedRate = 600000) // 10분마다 실행
+    public void cancelExpiredReservations() {
+        int expiredCount = mungShopReservationService.cancelExpiredReservations();
+        log.info("Expired reservations count: {}", expiredCount);
+    }
 }
